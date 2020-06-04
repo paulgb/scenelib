b8adf8ce3809eeebe8e45a6602f954536b787b21
Paul Butler <paulgb@gmail.com>
2020-06-04 18:30:46 -0400

    extrude works mostly

---
+++ examples/tower_extrude.rs
@@ -0,0 +1,35 @@
+#[macro_use]
+extern crate scenelib;
+use scenelib::prelude::*;
+
+fn main() {
+    let mut scene3d: Scene3 = Scene3::new();
+    let control_points = 20;
+    let resolution = 20;
+    let divisions = control_points * resolution;
+
+    let mut noise_maker = NoiseMaker::new(344.);
+    noise_maker.x_period(control_points);
+    let origin = pt(0., 0.);
+
+    for y in 0..100 {
+        let poly = Polygon::new(
+            (0..divisions)
+                .map(|i| {
+                    let r = noise_maker
+                        .noise(i as f64 / resolution as f64, y as f64 / resolution as f64);
+                    origin
+                        + (2. * r + 8.)
+                            * Vector::from_angle((i as f64 / divisions as f64) * 2. * PI)
+                })
+                .collect(),
+        );
+
+        let mut p3 = Form::extrude_from_poly(&poly, 0.1);
+        p3 = p3.scale(100.).translate(vec3(0., 0., 20. * y as f64));
+        scene3d.add_form(p3);
+    }
+
+    let scene = scene3d.camera_distance(8000.).to_2d();
+    scene.to_svg().save(&svg_filename!());
+}
--- src/projection/form.rs
+++ src/projection/form.rs
@@ -4,7 +4,12 @@ use crate::draw_mode::DrawMode;
 use crate::projection::apply::Apply;
 use crate::projection::polygon3::Polygon3;
 use crate::projection::transform::Transform;
+use crate::geom::polygon::Polygon;
+use crate::projection::types3::Point3;
+use crate::types::Point;
 use std::default::Default;
+use crate::projection::apply::{ApplyOps};
+use crate::projection::types3::Vector3;
 
 /// Represents a 3D shape.
 pub struct Form {
@@ -20,6 +25,45 @@ impl Form {
         Form {
             polys,
             draw_mode: Default::default(),
+        }
+    }
+
+    /// Create a form by extruding a polygon for a given height.
+    /// The resulting shape has the same X and Y values with Z values
+    /// taken from +/- the given height.
+    /// Note that this means the actual height of the shape is 2x the
+    /// given height. This is analogous to the way that a circle has
+    /// a diameter 2x the radius.
+    pub fn extrude_from_poly(poly: &Polygon, height: f64) -> Form {
+        let mut polys: Vec<Polygon3> = Vec::new();
+
+        let cap = Polygon3 { points: poly
+            .points
+            .0
+            .iter()
+            .map(|p: &Point| Point3::new(p.x, p.y, 0.0))
+            .collect()
+        };
+
+        polys.push(cap.clone().translate(Vector3::new(0., 0., height)));
+        polys.push(cap.translate(Vector3::new(0., 0., -height)));
+
+        for line in poly.points.line_segments() {
+            let p = Polygon3 {
+                points: vec![
+                    Point3::new(line.c1.x, line.c1.y, height),
+                    Point3::new(line.c2.x, line.c2.y, height),
+                    Point3::new(line.c2.x, line.c2.y, -height),
+                    Point3::new(line.c1.x, line.c1.y, -height),
+                ]
+            };
+
+            polys.push(p);
+        }
+
+        Form {
+            polys,
+            draw_mode: Default::default()
         }
     }
 
